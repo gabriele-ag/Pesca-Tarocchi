698d20e18eeb70cefa1e8ecc427c46e4de6b144d
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-11-07 19:35:28 +0100

    feat: init project defined data and api

---
+++ app/api/tarocchi/model/tarot.ts
@@ -0,0 +1,9 @@
+export type Tarot = {
+    id: number,
+    numero: number,
+    nome: string,
+    significato_dritto: string,
+    significato_rovesciato: string,
+    messaggio: string,
+
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+import "./globals.css";
+export default function Home() {
+  return (
+    <>
+    <h1 className="title">Ciao Tarocchi</h1>
+    </>
+  );
+}
