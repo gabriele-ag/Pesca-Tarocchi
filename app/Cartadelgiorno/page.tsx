9935017f5df214e9fb143b5ecb64313c7b62fd1c
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-11-10 11:14:38 +0100

    feat: created page CartaDelGiorno

--- app/Cartadelgiorno/page.tsx
+++ app/Cartadelgiorno/page.tsx
@@ -1,7 +1,31 @@
+'use client'
+
+import { useState } from "react"
+import { useTarot } from "../component/useTarot"
+
+import styles from "./CSS/cartadelgiorno.module.css"
+
 export default function CartaDelGiorno() {
+
+    const {tarots} = useTarot()
+
+    const [carta, setCarta] = useState<typeof tarots[0] | null>(null)
+
+    const pescaCarta = () => {
+        const cartaCasuale = Math.floor(Math.random() * tarots.length)
+        setCarta(tarots[cartaCasuale])
+    }
+
     return (
         <>
             <h1>Questa è la carta del giorno</h1>
+            <button className={styles.button} onClick={pescaCarta} disabled={!!carta}>Pesca una carta</button>
+            {carta && (
+                <div>
+                    <h2>{carta.nome}</h2>
+                    <p>{carta.messaggio}</p>
+                </div>
+            )}
         </>
     )
 }
