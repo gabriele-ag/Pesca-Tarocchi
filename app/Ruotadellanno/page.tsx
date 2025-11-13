3b73b007a4deed4974632e2f267d3b7030a74b38
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-11-13 10:21:29 +0100

    feat: fix logic on ruotadellanno

--- app/Ruotadellanno/page.tsx
+++ app/Ruotadellanno/page.tsx
@@ -9,13 +9,19 @@ import styles from "./CSS/ruotadellanno.module.css"
 
 export default function RuotaDelGiorno() {
     const { tarots } = useTarot()
-    
+
+        const [mazzo, setMazzo] = useState(tarots)
         const [carte, setCarta] = useState<Array<(typeof tarots)[number]>>([])
     
         const pescaCarta = () => {
             const cartaCasuale = Math.floor(Math.random() * tarots.length)
             const cartaPescata = tarots[cartaCasuale]
-            setCarta([...carte, cartaPescata])
+
+            if (carte.some(curCard => curCard.nome === cartaPescata.nome)) {
+                pescaCarta()
+            } else {
+                setCarta([...carte, cartaPescata])
+            }
         }
     
         const limiteCarte = carte.length >= 13
@@ -26,13 +32,13 @@ export default function RuotaDelGiorno() {
             <div className={styles.container}>
                 <h1 className={styles.title}>Stesa a tre carte</h1>
                 <p className={styles.descrizione}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae necessitatibus ipsa officia saepe tempora unde qui, quibusdam, cumque vitae natus, quidem odio eius repellendus eligendi doloribus quae. Facere, cupiditate dolorum!</p>
-                <button className={styles.button} onClick={pescaCarta} disabled={limiteCarte}>{carte.length === 13 ? 'Tutte le carte pescate' : 'Pesca carta'}</button>
+                <button className={styles.button} onClick={pescaCarta} disabled={limiteCarte}>{limiteCarte ? 'Tutte le carte pescate' : 'Pesca carta'}</button>
 
                 {carte.length > 0 && (
                     <div className={styles.boxCarte}>
                         {carte.map((curCard, index) => (
                             <div key={index}
-                            className={`$${styles.carta} ${index === centro ? styles.centrale : ""}`}>                                
+                            className={`${styles.carta} ${index === centro ? styles.centrale : ""}`}>                                
                                     <h2 className={styles.nomeCarta}>{curCard.nome}</h2>                               
                             </div>
                         ))}
