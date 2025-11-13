13ccec739ea4e397cdd90bc121189370d39a0180
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-11-13 09:43:42 +0100

    feat: created ruotadellanno logic

--- app/Ruotadellanno/page.tsx
+++ app/Ruotadellanno/page.tsx
@@ -1,7 +1,44 @@
+'use client'
+
+import { useState } from "react"
+import { useTarot } from "../component/useTarot"
+
+import styles from "./CSS/ruotadellanno.module.css"
+
+
+
 export default function RuotaDelGiorno() {
+    const { tarots } = useTarot()
+    
+        const [carte, setCarta] = useState<Array<(typeof tarots)[number]>>([])
+    
+        const pescaCarta = () => {
+            const cartaCasuale = Math.floor(Math.random() * tarots.length)
+            const cartaPescata = tarots[cartaCasuale]
+            setCarta([...carte, cartaPescata])
+        }
+    
+        const limiteCarte = carte.length >= 13
+        const centro = Math.floor(carte.length / 3)
+
     return (
-        <>
-            <h1>Questa è la ruota dell'anno</h1>
-        </>
+            <section>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Stesa a tre carte</h1>
+                <p className={styles.descrizione}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae necessitatibus ipsa officia saepe tempora unde qui, quibusdam, cumque vitae natus, quidem odio eius repellendus eligendi doloribus quae. Facere, cupiditate dolorum!</p>
+                <button className={styles.button} onClick={pescaCarta} disabled={limiteCarte}>{carte.length === 13 ? 'Tutte le carte pescate' : 'Pesca carta'}</button>
+
+                {carte.length > 0 && (
+                    <div className={styles.boxCarte}>
+                        {carte.map((curCard, index) => (
+                            <div key={index}
+                            className={`$${styles.carta} ${index === centro ? styles.centrale : ""}`}>                                
+                                    <h2 className={styles.nomeCarta}>{curCard.nome}</h2>                               
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </section>   
     )
 }
--- app/Stesaatre/page.tsx
+++ app/Stesaatre/page.tsx
@@ -12,7 +12,6 @@ export default function StesaATre() {
     const [carte, setCarta] = useState<Array<(typeof tarots)[number]>>([])
 
     const pescaCarta = () => {
-        if (carte.length >= 3) return 
         const cartaCasuale = Math.floor(Math.random() * tarots.length)
         const cartaPescata = tarots[cartaCasuale]
         setCarta([...carte, cartaPescata])
@@ -27,7 +26,7 @@ export default function StesaATre() {
             <div className={styles.container}>
                 <h1 className={styles.title}>Stesa a tre carte</h1>
                 <p className={styles.descrizione}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae necessitatibus ipsa officia saepe tempora unde qui, quibusdam, cumque vitae natus, quidem odio eius repellendus eligendi doloribus quae. Facere, cupiditate dolorum!</p>
-                <button className={styles.button} onClick={pescaCarta} disabled={limiteCarte}>Pesca le carte</button>
+                <button className={styles.button} onClick={pescaCarta} disabled={limiteCarte}>{carte.length === 3 ? 'Tutte le carte pescate' : 'Pesca carta'}</button>
 
                 {carte.length > 0 && (
                     <div className={styles.boxCarte}>
