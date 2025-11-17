2210e5da154fc9b24e9352a73881c19848ef4f31
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-11-17 10:47:16 +0100

    feat: added video background

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,16 @@ export default function Home() {
 
   return (
     <>
-    <section className="hero">
+    <section className="hero">    
+        <video 
+          autoPlay 
+          muted 
+          loop 
+          playsInline 
+          className="video-background"
+        >
+          <source src="/background.mp4" type="video/mp4"/>
+        </video>
       <div className="container">
         <h1 className="title-home">Pesca Tarocchi</h1>
         <p className="description">Scopri le carte <br />Rivela il tuo futuro</p>
