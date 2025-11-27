28c7e73795cbdc3c59b2eff759665544bc517430
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-11-27 08:53:13 +0100

    feat: updated project with API from backend

--- app/api/tarocchi/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { Tarot } from "./model/tarot";
-import tarotList from "../../data/tarotLIst.json";
-
-export function GET() {
-    const tarot: Tarot[] = tarotList
-    return NextResponse.json(tarot);
-}
--- app/component/useTarot.tsx
+++ app/component/useTarot.tsx
@@ -5,20 +5,27 @@ import { Tarot } from "../api/tarocchi/model/tarot";
 
 import { useState, useEffect } from "react";
 
+interface ApiResponse {
+    data: Tarot[],
+    count: number
+}
+
 export function useTarot() {
+
+    const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL
 
     const [tarots, setTarots] = useState<Tarot[]>([]);
     
     async function fetchTarot(): Promise<Tarot[]> {
         try {
-            const response = await fetch('/api/tarocchi');
+            const response = await fetch(`${apiUrl}`);
             if (!response.ok) {
                 throw new Error('Errore nel recupero dei tarocchi');
             }
             
-            const data: Tarot[] = await response.json()
-            setTarots(data)
-            return data
+            const results: ApiResponse = await response.json()
+            setTarots(results.data)
+            return results.data
         } catch (error) {
             console.error('Errore nel recupero dei tarocchi:', error)
             return [];
